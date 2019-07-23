da71d26318aa4f4eb9a65fe646776837ec5afa6b
Andrey Konovalov <andreyknvl@google.com>
2019-07-23 19:58:38 +0200

    UPSTREAM: arm64: untag user pointers in access_ok and __uaccess_mask_ptr
    (Upstream commit 2b835e24b5c6f9c633ff51973581ee7ca7b3e8ec).
    
    This patch is a part of a series that extends kernel ABI to allow to pass
    tagged user pointers (with the top byte set to something else other than
    0x00) as syscall arguments.
    
    copy_from_user (and a few other similar functions) are used to copy data
    from user memory into the kernel memory or vice versa. Since a user can
    provided a tagged pointer to one of the syscalls that use copy_from_user,
    we need to correctly handle such pointers.
    
    Do this by untagging user pointers in access_ok and in __uaccess_mask_ptr,
    before performing access validity checks.
    
    Note, that this patch only temporarily untags the pointers to perform the
    checks, but then passes them as is into the kernel internals.
    
    Reviewed-by: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    [will: Add __force to casting in untagged_addr() to kill sparse warning]
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Andrey Konovalov <andreyknvl@google.com>
    Bug: 135692346
    Change-Id: Ia97d1d311c2e2bf8e4584005b5085204db6d8955
    (cherry picked from commit e5358dd4d37c5fd0193d652eb798c9ecff431b83)

--- arch/arm64/include/asm/memory.h
+++ arch/arm64/include/asm/memory.h
@@ -175,7 +175,7 @@ static inline unsigned long kaslr_offset(void)
  * pass on to access_ok(), for instance.
  */
 #define untagged_addr(addr)	\
-	((__typeof__(addr))sign_extend64((u64)(addr), 55))
+	((__typeof__(addr))sign_extend64((__force u64)(addr), 55))
 
 /*
  * Physical vs virtual RAM address space conversion.  These are
