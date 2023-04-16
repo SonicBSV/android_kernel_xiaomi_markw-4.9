eab01361a8253d479a3b314107184279da9239c4
me-cafebabe <me.cafebabe@gmail.com>
2023-04-16 23:33:33 +0800

    markw: ts: Add sysctl interface for controlling touchscreen features
    Change-Id: Idcccc41aee5b11c3652e32b429bc0f6acc097e6b

--- touchscreen/include/xiaomi/touchscreen.h
+++ touchscreen/include/xiaomi/touchscreen.h
@@ -3,4 +3,16 @@
 
 extern bool xiaomi_touchscreen_is_probed;
 
+#if IS_ENABLED(CONFIG_TOUCHSCREEN_SYSCTL)
+#include <linux/device.h>
+
+struct xiaomi_touchscreen_operations_t {
+	struct device *dev;
+	int (*enable_dt2w)(struct device *dev, bool enable);
+	int (*disable_keys)(struct device *dev, bool disable);
+};
+
+extern int xiaomi_touchscreen_register_operations(struct xiaomi_touchscreen_operations_t *ts_ops);
+#endif
+
 #endif
