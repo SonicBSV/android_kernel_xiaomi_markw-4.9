8ebdee7305e42a000c9b68f8d675a8ade9bd846a
SonicBSV <sonicbsv@gmail.com>
2023-12-17 21:36:29 +0300

    markw: fs: pstore: Import xiaomi changes
    Co-authored-by: Slava Vidanov @anomalchik
    Co-authored-by: Abhishek Aggarwal @TheScarastic

--- fs/pstore/ram.c
+++ fs/pstore/ram.c
@@ -45,7 +45,10 @@ module_param(record_size, ulong, 0400);
 MODULE_PARM_DESC(record_size,
 		"size of each dump done on oops/panic");
 
-#if defined(CONFIG_MACH_XIAOMI_C6) || defined(CONFIG_MACH_XIAOMI_MARKW) || defined(CONFIG_MACH_XIAOMI_D2)
+#ifdef CONFIG_MACH_XIAOMI_MARKW
+static ulong ramoops_console_size = 256*1024UL;
+#endif
+#if defined(CONFIG_MACH_XIAOMI_C6) || defined(CONFIG_MACH_XIAOMI_D2)
 static ulong ramoops_console_size = 512*1024UL;
 #else
 static ulong ramoops_console_size = MIN_MEM_SIZE;
@@ -57,7 +60,7 @@ static ulong ramoops_ftrace_size = MIN_MEM_SIZE;
 module_param_named(ftrace_size, ramoops_ftrace_size, ulong, 0400);
 MODULE_PARM_DESC(ftrace_size, "size of ftrace log");
 
-#if defined(CONFIG_MACH_XIAOMI_C6) || defined(CONFIG_MACH_XIAOMI_MARKW) || defined(CONFIG_MACH_XIAOMI_D2)
+#if defined(CONFIG_MACH_XIAOMI_C6) || defined(CONFIG_MACH_XIAOMI_D2)
 static ulong ramoops_pmsg_size = 32*1024UL;
 #else
 static ulong ramoops_pmsg_size = MIN_MEM_SIZE;
