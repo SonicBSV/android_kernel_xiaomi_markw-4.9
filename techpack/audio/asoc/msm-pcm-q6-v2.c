872ae06a8fcfe3ef9aa2aaca2ee2fe86a186db0a
SonicBSV <sonicbsv@gmail.com>
2023-12-21 07:51:44 +0300

    Revert "techpack: audio: forward port runtime q6 avs version detection from 3.18"
    This reverts commit 103f8fc9f7c0d6b545ae50a39555de78fc866688.

--- techpack/audio/asoc/msm-pcm-q6-v2.c
+++ techpack/audio/asoc/msm-pcm-q6-v2.c
@@ -388,10 +388,11 @@ static int msm_pcm_playback_prepare(struct snd_pcm_substream *substream)
 			return -ENOMEM;
 		}
 	} else {
-		if (q6core_get_avs_version() < Q6_SUBSYS_AVS2_8)
-			ret = q6asm_open_write_v3(prtd->audio_client,
-				fmt_type, bits_per_sample);
-		else if (q6core_get_avcs_api_version_per_service(
+#ifdef CONFIG_MACH_XIAOMI_C6
+		ret = q6asm_open_write_v3(prtd->audio_client,
+			fmt_type, bits_per_sample);
+#else
+		if (q6core_get_avcs_api_version_per_service(
 				APRV2_IDS_SERVICE_ID_ADSP_ASM_V) >=
 				ADSP_ASM_API_VERSION_V2)
 			ret = q6asm_open_write_v5(prtd->audio_client,
@@ -399,6 +400,7 @@ static int msm_pcm_playback_prepare(struct snd_pcm_substream *substream)
 		else
 			ret = q6asm_open_write_v4(prtd->audio_client,
 				fmt_type, bits_per_sample);
+#endif
 
 		if (ret < 0) {
 			pr_err("%s: q6asm_open_write failed (%d)\n",
@@ -439,13 +441,14 @@ static int msm_pcm_playback_prepare(struct snd_pcm_substream *substream)
 			prtd->channel_map, bits_per_sample);
 	} else {
 
-		if (q6core_get_avs_version() < Q6_SUBSYS_AVS2_8) {
-			ret = q6asm_media_format_block_multi_ch_pcm_v3(
+#ifdef CONFIG_MACH_XIAOMI_C6
+		ret = q6asm_media_format_block_multi_ch_pcm_v3(
 				prtd->audio_client, runtime->rate,
 				runtime->channels, !prtd->set_channel_map,
 				prtd->channel_map, bits_per_sample,
 				sample_word_size);
-		} else if (q6core_get_avcs_api_version_per_service(
+#else
+		if (q6core_get_avcs_api_version_per_service(
 				APRV2_IDS_SERVICE_ID_ADSP_ASM_V) >=
 				ADSP_ASM_API_VERSION_V2) {
 
@@ -463,6 +466,7 @@ static int msm_pcm_playback_prepare(struct snd_pcm_substream *substream)
 				sample_word_size, ASM_LITTLE_ENDIAN,
 				DEFAULT_QF);
 		}
+#endif
 	}
 	if (ret < 0)
 		pr_info("%s: CMD Format block failed\n", __func__);
@@ -521,11 +525,11 @@ static int msm_pcm_capture_prepare(struct snd_pcm_substream *substream)
 				__func__, params_channels(params),
 				prtd->audio_client->perf_mode);
 
-		if (q6core_get_avs_version() < Q6_SUBSYS_AVS2_8)
-			ret = q6asm_open_read_v3(prtd->audio_client,
-				FORMAT_LINEAR_PCM,
+#ifdef CONFIG_MACH_XIAOMI_C6
+		ret = q6asm_open_read_v3(prtd->audio_client, FORMAT_LINEAR_PCM,
 				bits_per_sample);
-		else if (q6core_get_avcs_api_version_per_service(
+#else
+		if (q6core_get_avcs_api_version_per_service(
 				APRV2_IDS_SERVICE_ID_ADSP_ASM_V) >=
 				ADSP_ASM_API_VERSION_V2)
 			ret = q6asm_open_read_v5(prtd->audio_client,
@@ -535,6 +539,7 @@ static int msm_pcm_capture_prepare(struct snd_pcm_substream *substream)
 			ret = q6asm_open_read_v4(prtd->audio_client,
 				FORMAT_LINEAR_PCM,
 				bits_per_sample, false);
+#endif
 		if (ret < 0) {
 			pr_err("%s: q6asm_open_read failed\n", __func__);
 			q6asm_audio_client_free(prtd->audio_client);
@@ -602,14 +607,14 @@ static int msm_pcm_capture_prepare(struct snd_pcm_substream *substream)
 			__func__, prtd->samp_rate, prtd->channel_mode,
 			bits_per_sample, sample_word_size);
 
-	if (q6core_get_avs_version() < Q6_SUBSYS_AVS2_8)
-		ret = q6asm_enc_cfg_blk_pcm_format_support_v3(
-						prtd->audio_client,
-						prtd->samp_rate,
-						prtd->channel_mode,
-						bits_per_sample,
-						sample_word_size);
-	else if (q6core_get_avcs_api_version_per_service(
+#ifdef CONFIG_MACH_XIAOMI_C6
+	ret = q6asm_enc_cfg_blk_pcm_format_support_v3(prtd->audio_client,
+						      prtd->samp_rate,
+						      prtd->channel_mode,
+						      bits_per_sample,
+						      sample_word_size);
+#else
+	if (q6core_get_avcs_api_version_per_service(
 			APRV2_IDS_SERVICE_ID_ADSP_ASM_V) >=
 			ADSP_ASM_API_VERSION_V2)
 		ret = q6asm_enc_cfg_blk_pcm_format_support_v5(
@@ -630,6 +635,7 @@ static int msm_pcm_capture_prepare(struct snd_pcm_substream *substream)
 						ASM_LITTLE_ENDIAN,
 						DEFAULT_QF);
 
+#endif
 	if (ret < 0)
 		pr_debug("%s: cmd cfg pcm was block failed", __func__);
 
